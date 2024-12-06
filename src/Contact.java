dc387a70b4136910599f59f8c3e9257589c90d8c
BL-Arpit <arpit.sahu@bridgelabz.com>
2024-12-06 17:36:53 +0530

    Added : Feature to add Contacts

---
+++ src/Contact.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Contact {
+    private String name;
+    private String email;
+    private String state;
+    private int phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Contact(String name, String email, String state, int phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.state = state;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public static Contact createContact(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name");
+        String name  = sc.next();
+        System.out.println("Email");
+        String email  = sc.next();
+        System.out.println("State");
+        String state  = sc.next();
+        System.out.println("PhoneNumber");
+        int phone  = sc.nextInt();
+
+        return new Contact(name,email,state,phone);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", state='" + state + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+}
+//it is not secured - any thing outside the object have the ability to change and the scope lies outsid / we use getters and setter
+
+//We dont need a generic in contact - because it is the lowest fundamental object - Based on Contact So we cant modify the contact any more - It should be treated as a Struct - but since our code Requires an interface to work - we have to implenet it
